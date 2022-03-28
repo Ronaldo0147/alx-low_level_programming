ee2fc12e3bc5d1fcba1373b248384f1cdee7de4f
Abdulkudus <abdulkudusakanji777@gmail.com>
2022-03-28 13:00:51 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-#ifndef holberton_h
-#define holberton_h
-=======
-#ifndef main.h
-#define main.h
->>>>>>> 2def80c4d9d32a2494c4de6b4ef7dfaf27ef0743
-
-int _putchar(char c);
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -16,5 +8,3 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-#endif
